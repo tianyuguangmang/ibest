88de10e94916340469ac40796c0c1b5464e8123a
tygm <18806524037@163.com>
2018-02-05 20:44:56 +0800

    tygm

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -32,19 +32,19 @@ Page({
       }
     }
   },
-  onLoad:function(){
-    this.setData({
-      menuList:[{id:1,name:"今日推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"}],
-      bannerList:[{pic:'https://ps.ssl.qhimg.com/sdmt/179_135_100/t010b0a4aa5bb6941c4.jpg'}],
-      cartList:[{
-        id:1,
-        title:'特价特价商品特价商特价',
-        mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-        originPrice:40,
-        currentPrice:20,
-        sku:"特价商品黑色",
-        count:1,
-      }]
+  onShow:function(){
+    var _this = this;
+    wx.getStorage({
+      key: 'cart_info',
+      success: function(res) {
+          console.log("x",res);
+          
+          _this.setData({
+            cartList:res.data,
+          })
+      } 
     })
+  },
+  onLoad:function(){
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,23 @@ import * as Size from '../../js/imagesize';
 Page({
 	data: {
 		Size,
+		menuList:[{id:1,name:"今日推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"}],
+		bannerList:[{pic:'https://ps.ssl.qhimg.com/sdmt/179_135_100/t010b0a4aa5bb6941c4.jpg'}],
+		goodsList:[{
+			productId:1,
+			title:'特价商品价商',
+			mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
+			currentPrice:10,
+			sellCount:10,
+			originPrice:20
+		},{
+			productId:2,
+			title:'特价商品价商',
+			mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
+			currentPrice:10,
+			sellCount:10,
+			originPrice:20
+		}]
 		
 	},
 
@@ -32,59 +49,127 @@ Page({
 			}
 		}
 	},
-	onLoad:function(){
-		this.setData({
-			menuList:[{id:1,name:"今日推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"},{id:1,name:"推荐"}],
-			bannerList:[{pic:'https://ps.ssl.qhimg.com/sdmt/179_135_100/t010b0a4aa5bb6941c4.jpg'}],
-			goodsList:[{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			},{
-				id:1,
-				title:'特价商品',
-				mainImage:'https://img.alicdn.com/tfs/TB1EkSvdr_I8KJjy1XaXXbsxpXa-350-350.jpg_240x240xz.jpg_.webp',
-				unitPrice:10,
-				marketPrice:20
-			}]
+	cartInfo:{},
+	/**
+	 * 购物车本地存储数据格式 
+	 * key: mid 商户id uid 用户id
+	 * cart_info
+	 * 
+	 * value:
+	 * pid 商品id skuid 商品规格id
+	 * {
+	 * 		pid_1_skuid_1:{
+	 * 			productId:1,//商品id
+	 * 			count:1,//数量
+	 * 			title:'',//名称
+	 * 			currentPrice:10,//当前价格
+	 * 			mainImage:'',
+	 * 			originPrice:10,
+	 * 			sku:褐色,
+	 * 			skuId:1
+	 * 		}
+	 * 
+	 * 
+	 * }
+	 * @param {[type]} currentTarget [description]
+	 */
+	cartList:function(){
+		var _list = this.data.goodsList;
+		var _cartInfo = {};
+		for(var i=0,_len = _list.length;i<_len;i++){
+			if(_list[i].count&&_list[i].count>0){
+				_cartInfo["pid_1_skuid_1"] = {
+
+				}
+
+			}
+		}
+	},
+	/**
+	 * 计算购物车
+	 * @param  {[type]} data 商品数据
+	 * @return {[type]}
+	 */
+	calcCart:function(data){
+		var _cartInfo = this.cartInfo;
+		if(_cartInfo['pid_'+data.productId+'_skuid_1']){
+			if(data.count == 0){
+				delete _cartInfo['pid_'+data.productId+'_skuid_1'];
+			}else{
+				_cartInfo['pid_'+data.productId+'_skuid_1'].count = data.count;
+			}
+		}else{
+			_cartInfo['pid_'+data.productId+'_skuid_1'] = {
+				count:data.count,
+				productId:data.productId,
+				title:data.title,//名称
+				currentPrice:data.currentPrice,
+				mainImage:data.mainImage,
+				originPrice:data.originPrice,
+				sku:'褐色',
+				skuId:0
+			}
+		}
+
+	},
+	onHide:function(){
+		console.log(this.cartInfo);
+		wx.setStorage({
+		  key:"cart_info",
+		  data:this.cartInfo
 		})
+
+	},
+	/**
+	 * 购物车增加
+	 * @param {[type]} currentTarget [description]
+	 */
+	addToCart:function(currentTarget){
+		var _index = app.getData(currentTarget,'index');
+		var _list = this.data.goodsList;
+		
+		_list[_index].count = _list[_index].count?_list[_index].count+1:1;
+		var _select = _list[_index];
+		this.calcCart(_select);
+
+
+		this.setData({
+			goodsList:_list
+		})
+	},
+	/**
+	 * 购物车减少
+	 * @param  {[type]} currentTarget [description]
+	 * @return {[type]}               [description]
+	 */
+	subtractCart:function(currentTarget){
+		var _index = app.getData(currentTarget,'index');
+		var _list = this.data.goodsList;
+		_list[_index].count = _list[_index].count>0?_list[_index].count-1:0;
+		var _select = _list[_index];
+		this.calcCart(_select);
+		this.setData({
+			goodsList:_list
+		})
+	},
+	onLoad:function(){
+		var _this = this;
+		wx.getStorage({
+		  key: 'cart_info',
+		  success: function(res) {
+	      var _cartInfo = res.data;
+	      _this.cartInfo = _cartInfo;
+	      var _goodsList = _this.data.goodsList;
+	      for(var i=0,_len = _goodsList.length;i<_len;i++){
+	      	if(_cartInfo['pid_'+_goodsList[i].productId+'_skuid_1']){
+	      		_goodsList[i].count = _cartInfo['pid_'+_goodsList[i].productId+'_skuid_1'].count;
+	      	}
+	      }
+	      _this.setData({
+	      	goodsList:_goodsList,
+	      })
+		  } 
+		})
+		
 	}
 })
