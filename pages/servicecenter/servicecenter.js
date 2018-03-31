d1fb3d4ab43bfe9ffec064e87fb74171169e87f9
tygm <18806524037@163.com>
2018-03-31 18:48:42 +0800

    tygm

---
+++ pages/servicecenter/servicecenter.js
@@ -0,0 +1,53 @@
+/**
+ * @author tygm
+ * @Date 2018/3/20
+ */
+var service = require("../../js/service.js");
+var app = getApp();
+import * as Size from '../../js/imagesize';
+Page({
+	data: {
+		Size,
+		noMoreData: false,
+		hotList: null
+	},
+	timeCount:function(s){
+    var _this = this;
+    var _results = s||this.data.effectiveTime;
+    //disEndTime
+    var _timer = setInterval(function(){
+      
+        if(_results>0){
+          _results -=1;
+        }else{
+          clearInterval(_timer);
+        }
+      _this.setData({
+        effectiveTime:_results
+        
+      })
+
+    },1000)
+  },
+	//页面分享功能
+	onShareAppMessage: function(res) {
+
+		return {
+			//longitude 经度 
+			//latitude 维度
+			title: app.globalData.title,
+			path: '/pages/mall/mall',
+			success: function(res) {
+				// 转发成功
+				wx.showToast({
+					title: '转发成功',
+					icon: 'success',
+					duration: 2000
+				})
+			},
+			fail: function(res) {
+
+			}
+		}
+	}
+})
